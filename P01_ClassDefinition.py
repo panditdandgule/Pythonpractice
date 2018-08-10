ecc6344dc5140377cb5478925587ca845e0e5f13
Pandit Dandgule <panditdandgule777@gmail.com>
2018-08-10 08:43:57 +0530

    Basic class defination and object creation.

---
+++ P01_ClassDefinition.py
@@ -0,0 +1,14 @@
+#Author: PANDIT DANDGULE
+#In this program we will see how to define a class
+
+class MyFirstClass():
+    #class Attributes
+    var=10
+
+firstObject=MyFirstClass()
+print(firstObject) #printing object memory hex
+print(firstObject.var) #Accessing Class Attributes
+
+secondObject=MyFirstClass()
+print(secondObject)
+print(secondObject.var)
